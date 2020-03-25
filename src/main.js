52656e3d653f08c5e8d6ab1f84836e43c5acc926
Abay <kettleofrassel@gmail.com>
2020-03-25 05:12:44 -0700

    drawing random lines

--- src/main.js
+++ src/main.js
@@ -1,7 +1,33 @@
 // Template
 
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+// selecting canvas element 500x500
+const canvas = document.getElementById("canvas");
+const midX = parseInt(canvas.clientWidth / 2);
+const midY = parseInt(canvas.clientHeight / 2);
+
+// getting context
+const context = canvas.getContext("2d");
+
+// Actual drawing
+
+// center the canvas
+context.moveTo(midX, midY);
+
+function lineFromCenter(x, y) {
+  context.lineTo(midX + x, midY + y);
+}
+
+function randomNumber(a, b) {
+  const sum = Math.abs(a) + Math.abs(b);
+
+  return parseInt(Math.random() * sum) - Math.abs(a);
+}
+
+function drawRandom(a, b) {
+  lineFromCenter(randomNumber(a, b), randomNumber(a, b));
+  context.stroke();
+}
+
+setInterval(() => drawRandom(-250, 250), 200);
+
+context.stroke();
