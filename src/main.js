be6905ef75318729354f6b6e754161c773fcfaaa
Abay <kettleofrassel@gmail.com>
2020-03-25 05:50:38 -0700

    random circles

--- src/main.js
+++ src/main.js
@@ -7,12 +7,14 @@ const midx = parseInt(canvas.clientWidth / 2);
 const midy = parseInt(canvas.clientHeight / 2);
 
 context.moveTo(midx, midy);
-context.strokeRect(midx, midy, 10, 10);
+
+context.beginPath();
+context.arc(10, 10, 20, 0, 2 * Math.PI);
 
 function randomizeInterval(a, b) {
   const length = Math.abs(a) + Math.abs(b);
 
-  return parseInt(Math.random() * length);
+  return parseInt(Math.random() * length) + Math.abs(a);
 }
 
 function randomizeNumber(number) {
@@ -20,12 +22,15 @@ function randomizeNumber(number) {
 }
 
 setInterval(function() {
-  context.strokeRect(
+  context.beginPath();
+  context.arc(
     randomizeNumber(canvas.clientWidth),
     randomizeNumber(canvas.clientHeight),
-    randomizeNumber(50),
-    randomizeNumber(50)
+    randomizeInterval(10, 50), // radius
+    0, // start angle
+    2 * Math.PI // end angle
   );
+  context.stroke();
 }, 200);
 
 context.stroke();
